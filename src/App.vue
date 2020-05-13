3d4d831c4e12d74e0b58541205c659659f33e59e
lucasltinoco <lucasltinoco@gmail.com>
2020-05-13 17:48:19 -0300

    putting the components directly into the #app

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <template>
   <div id="app">
-    <Content />
+    <Home />
+    <WhoIAm />
+    <WhatIDo />
+    <MyProjects />
+    <ContactMe />
+    <Footer />
   </div>
 </template>
 
 <script>
-import Content from "@/components/Content";
+import Home from "./components/Home";
+import WhoIAm from "./components/WhoIAm";
+import WhatIDo from "./components/WhatIDo";
+import MyProjects from "./components/MyProjects";
+import ContactMe from "./components/ContactMe";
+import Footer from "./components/Footer";
 
 export default {
   name: "App",
   components: {
-    Content
+    Home,
+    WhoIAm,
+    WhatIDo,
+    MyProjects,
+    ContactMe,
+    Footer
   }
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Jost:wght@200&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Jost:wght@200&display=swap");
 
 * {
   font-family: "Jost", sans-serif;
@@ -30,6 +45,9 @@ body {
   margin: 0px;
   color: #000;
   background: linear-gradient(to left, #ece9e6, #ffffff);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 #app {
@@ -37,9 +55,24 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   margin: 0px;
-  height: 100vh;
+  width: 75vw;
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
   align-items: center;
+}
+
+h1 {
+  position: sticky;
+  top: 5vh;
+  font-size: 2.5rem;
+  font-weight: 900;
+}
+
+.fa-chevron-down,
+.fa-chevron-down:hover {
+  padding-bottom: 10vh;
+  text-decoration: none;
+  color: #000;
 }
 </style>
