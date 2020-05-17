890cd63bbdb2d805d0acf6007ec2c4b8cc0902a9
lucasltinoco <lucasltinoco@gmail.com>
2020-05-17 17:57:32 -0300

    implementing basic structure of contact me

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -1,18 +1,59 @@
 <template>
   <div id="contact-me">
     <h1>contact me!</h1>
-      <p>Contact Me Content</p>
-      <a href="https://www.linkedin.com/in/lucas-tinoco-783420194/">Linkedin</a>
-      <a href="https://github.com/lucasltinoco">GitHub</a>
-      <a href="mailto:lucasltinoco@gmail.com">E-mail</a>
-      <a href="#home" class="fa fa-chevron-up"></a>
+    <b-form @submit="onSubmit" @reset="onReset">
+      <b-form-group id="input-group-1" label="Subject:" label-for="input-1">
+        <b-form-input
+          id="input-1"
+          v-model="form.subject"
+          type="subject"
+          required
+          placeholder="Enter subject"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Text:" label-for="input-2">
+        <b-form-textarea
+          id="input-2"
+          v-model="form.body"
+          required
+          placeholder="Enter text"
+          rows="6"
+          max-rows="12"
+        ></b-form-textarea>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Send me!</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <a href="#home" class="fa fa-chevron-up"></a>
   </div>
 </template>
 
 <script>
 export default {
-  name: "MyProjects"
-}
+  name: "MyProjects",
+  data: function() {
+    return {
+      form: {
+        subject: "",
+        body: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      document.location.href = 
+        `mailto:lucasltinoco@gmail.com?subject=${this.form.subject}&body=${this.form.body}`
+    },
+    onReset(e) {
+      e.preventDefault();
+      this.form.subject = "";
+      this.form.body = "";
+    }
+  }
+};
 </script>
 
 <style>
@@ -23,8 +64,13 @@ export default {
   align-items: center;
   height: 100vh;
 }
-#contact-me p {
-  font-size: 1.5rem;
+
+#contact-me b-form-group {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
 }
 
 .fa-chevron-up,
