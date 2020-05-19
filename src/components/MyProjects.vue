e1bc3ae6fc5b574568be52ac8b27fa90e94ffa61
lucasltinoco <lucasltinoco@gmail.com>
2020-05-19 16:11:12 -0300

    implementing tags logic

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -1,67 +1,119 @@
 <template>
   <div id="my-projects">
     <h1>my projects</h1>
-    <div class="tags">tags</div>
+    <div class="tags">
+      <b-button-group size="sm">
+        <b-button @click="setTag('cpp')">C++</b-button>
+        <b-button @click="setTag('allegro')">Allegro</b-button>
+        <b-button @click="setTag('vue')">Vue</b-button>
+        <b-button @click="setTag('bootstrap')">Bootstrap</b-button>
+        <b-button @click="setTag('mongodb')">MongoDB</b-button>
+        <b-button @click="setTag('postgresql')">PostgreSQL</b-button>
+        <b-button @click="setTag('jquery')">jQuery</b-button>
+        <b-button @click="setTag('webpack')">WebPack</b-button>
+        <b-button @click="setTag('react')">React</b-button>
+        <b-button @click="setTag('jsonserver')">JSON Server</b-button>
+        <b-button @click="setTag('angular')">Angular</b-button>
+        <b-button @click="setTag('all')">All</b-button>
+      </b-button-group>
+    </div>
     <div class="projects">
-
-      <div class="card proerd">
+      <div class="card proerd" v-show="tags.cpp || tags.allegro || tags.all">
         <div class="card-category">Run & Jump Game</div>
         <a class="card-link" href="https://github.com/lucasltinoco/proerd-game"></a>
       </div>
 
-      <div class="card knowledge">
+      <div class="card knowledge" v-show="tags.vue || tags.bootstrap || tags.mongodb || tags.postgresql || tags.all">
         <div class="card-category">Knowledge Base</div>
         <a class="card-link" href="https://github.com/lucasltinoco/knowledge"></a>
       </div>
 
-      <div class="card gallery">
+      <div class="card gallery" v-show="tags.bootstrap || tags.jquery || tags.webpack || tags.all">
         <div class="card-category">Trip Gallery</div>
-        <a class="card-link" href="https://github.com/lucasltinoco/cursoWeb/tree/master/bootstrap/projeto-galeria"></a>
+        <a
+          class="card-link"
+          href="https://github.com/lucasltinoco/cursoWeb/tree/master/bootstrap/projeto-galeria"
+        ></a>
       </div>
 
-      <div class="card react-calc">
+      <div class="card react-calc" v-show="tags.react || tags.all">
         <div class="card-category">React Calculator</div>
-        <a class="card-link" href="https://github.com/lucasltinoco/cursoWeb/tree/master/react/calculadora"></a>
+        <a
+          class="card-link"
+          href="https://github.com/lucasltinoco/cursoWeb/tree/master/react/calculadora"
+        ></a>
       </div>
 
-      <div class="card react-crud">
+      <div class="card react-crud" v-show="tags.react || tags.jsonserver || tags.all">
         <div class="card-category">React CRUD</div>
         <a class="card-link" href="https://github.com/lucasltinoco/cursoWeb/tree/master/react/crud"></a>
       </div>
 
-      <div class="card qr-code">
+      <div class="card qr-code" v-show="tags.vue || tags.all">
         <div class="card-category">QR Code Generator</div>
         <a class="card-link" href="https://github.com/lucasltinoco/cursoWeb/tree/master/vue/qrcode"></a>
       </div>
 
-      <div class="card vue-calc">
+      <div class="card vue-calc" v-show="tags.vue || tags.all">
         <div class="card-category">Vue Calculator</div>
-        <a class="card-link" href="https://github.com/lucasltinoco/cursoWeb/tree/master/vue/calculator"></a>
+        <a
+          class="card-link"
+          href="https://github.com/lucasltinoco/cursoWeb/tree/master/vue/calculator"
+        ></a>
       </div>
 
-      <div class="card monty-hall">
+      <div class="card monty-hall" v-show="tags.vue || tags.all">
         <div class="card-category">Monty Hall Problem</div>
-        <a class="card-link" href="https://github.com/lucasltinoco/cursoWeb/tree/master/vue/montyhall"></a>
+        <a
+          class="card-link"
+          href="https://github.com/lucasltinoco/cursoWeb/tree/master/vue/montyhall"
+        ></a>
       </div>
 
-      <div class="card angular-crud">
+      <div class="card angular-crud" v-show="tags.angular || tags.jsonserver || tags.all">
         <div class="card-category">Angular Crud</div>
         <a class="card-link" href="https://github.com/lucasltinoco/cursoWeb/tree/master/angular"></a>
       </div>
 
-      <div class="card portfolio">
+      <div class="card portfolio" v-show="tags.vue || tags.bootstrap || tags.all">
         <div class="card-category">Portfolio</div>
         <a class="card-link" href="https://github.com/lucasltinoco/my-portfolio"></a>
       </div>
-
     </div>
+
     <a href="#contact-me" class="fa fa-chevron-down"></a>
   </div>
 </template>
 
 <script>
 export default {
-  name: "MyProjects"
+  name: "MyProjects",
+  data: function() {
+    return {
+      tags: {
+        cpp: false,
+        allegro: false,
+        vue: false,
+        bootstrap: false,
+        mongodb: false,
+        postgresql: false,
+        jquery: false,
+        webpack: false,
+        react: false,
+        jsonserver: false,
+        angular: false,
+        all: true
+      }
+    };
+  },
+  methods: {
+    setTag(tag) {
+      for (let [key, value] of Object.entries(this.tags)) {
+        key == tag ? value = true : value = false 
+        this.tags[key] = value
+      }
+    }
+  }
 };
 </script>
 
@@ -174,5 +226,4 @@ export default {
   background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)),
     url("../assets/imgs/projects/proerd.gif"); /* TEMPORARY */
 }
-
 </style>
