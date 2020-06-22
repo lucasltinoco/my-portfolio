04420cc50b082e9f65bb988b83d630210ad49682
lucasltinoco <lucasltinoco@gmail.com>
2020-06-22 12:03:45 -0300

    adjusting splash screen timeout to 1s

--- src/App.vue
+++ src/App.vue
@@ -164,7 +164,7 @@ export default {
       setTimeout(() => {
         this.pageIsLoading = false
         this.redirectUrl();
-      }, 250);
+      }, 1000);
     },
     playVideos() {
       const videos = document.querySelectorAll(
